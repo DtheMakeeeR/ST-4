3e65685300bbcd15426593852ce6805fd8d615fd
DtheMakeeeR <danilgol45@mail.ru>
2026-04-13 09:28:43 +0300

    init

---
+++ BugPro/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stateless;
+
+namespace BugPro
+{
+    public enum BugState
+    {
+        New,
+        Analysis,
+        MoreInfo,
+        Fixed,
+        NotBug,
+        NoFix,
+        Duplicate,
+        NotRepro,
+        Separate,
+        NoTime,
+        Other
+    }
+
+    public enum BugTrigger
+    {
+        Start,
+        AddInfo,
+        Fix,
+        SetNotBug,
+        SetNoFix,
+        SetDuplicate,
+        SetNotRepro,
+        RequestInfo,
+        SetSeparate,
+        SetNoTime,
+        SetOther
+    }
+
+    public class Bug
+    {
+        private StateMachine<BugState, BugTrigger> machine;
+        public BugState State => machine.State;
+
+        public Bug()
+        {
+            machine = new StateMachine<BugState, BugTrigger>(BugState.New);
+
+            machine.Configure(BugState.New)
+                .Permit(BugTrigger.Start, BugState.Analysis);
+
+            machine.Configure(BugState.Analysis)
+                .Permit(BugTrigger.RequestInfo, BugState.MoreInfo)
+                .Permit(BugTrigger.Fix, BugState.Fixed)
+                .Permit(BugTrigger.SetNotBug, BugState.NotBug)
+                .Permit(BugTrigger.SetNoFix, BugState.NoFix)
+                .Permit(BugTrigger.SetDuplicate, BugState.Duplicate)
+                .Permit(BugTrigger.SetNotRepro, BugState.NotRepro)
+                .Permit(BugTrigger.SetSeparate, BugState.Separate)
+                .Permit(BugTrigger.SetNoTime, BugState.NoTime)
+                .Permit(BugTrigger.SetOther, BugState.Other);
+
+            machine.Configure(BugState.MoreInfo)
+                .Permit(BugTrigger.AddInfo, BugState.Analysis);
+
+            machine.Configure(BugState.Fixed);
+            machine.Configure(BugState.NotBug);
+            machine.Configure(BugState.NoFix);
+            machine.Configure(BugState.Duplicate);
+            machine.Configure(BugState.NotRepro);
+            machine.Configure(BugState.Separate);
+            machine.Configure(BugState.NoTime);
+            machine.Configure(BugState.Other);
+        }
+
+        public void Start() => machine.Fire(BugTrigger.Start);
+        public void AddInfo() => machine.Fire(BugTrigger.AddInfo);
+        public void Fix() => machine.Fire(BugTrigger.Fix);
+        public void SetNotBug() => machine.Fire(BugTrigger.SetNotBug);
+        public void SetNoFix() => machine.Fire(BugTrigger.SetNoFix);
+        public void SetDuplicate() => machine.Fire(BugTrigger.SetDuplicate);
+        public void SetNotRepro() => machine.Fire(BugTrigger.SetNotRepro);
+        public void RequestInfo() => machine.Fire(BugTrigger.RequestInfo);
+        public void SetSeparate() => machine.Fire(BugTrigger.SetSeparate);
+        public void SetNoTime() => machine.Fire(BugTrigger.SetNoTime);
+        public void SetOther() => machine.Fire(BugTrigger.SetOther);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bug = new Bug();
+            Console.WriteLine($"Начальное состояние: {bug.State}");
+
+            bug.Start();
+            Console.WriteLine($"После Start: {bug.State}");
+
+            bug.Fix();
+            Console.WriteLine($"После Fix: {bug.State}");
+
+            try
+            {
+                bug.SetDuplicate();
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Исключение: {ex.Message}");
+            }
+
+            Console.WriteLine("Завершено.");
+        }
+    }
+
+}
---
+++ BugTest/TestUnit1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BugPro;
+
+namespace Tests
+{
+    
+    [TestClass]
+    public class BugTests
+    {
+        private static Bug InAnalysis()
+        {
+            var bug = new Bug();
+            bug.Start();
+            return bug;
+        }
+
+        private static Bug InNeedInfo()
+        {
+            var bug = InAnalysis();
+            bug.RequestInfo();
+            return bug;
+        }
+
+        [TestMethod]
+        public void New_State_IsNew()
+        {
+            var bug = new Bug();
+            Assert.AreEqual(BugState.New, bug.State);
+        }
+
+        [TestMethod]
+        public void Start_FromNew_ToAnalysis()
+        {
+            var bug = new Bug();
+            bug.Start();
+            Assert.AreEqual(BugState.Analysis, bug.State);
+        }
+
+        [TestMethod]
+        public void NeedInfo_FromAnalysis()
+        {
+            var bug = InAnalysis();
+            bug.RequestInfo();
+            Assert.AreEqual(BugState.MoreInfo, bug.State);
+        }
+
+        [TestMethod]
+        public void AddInfo_BackToAnalysis()
+        {
+            var bug = InNeedInfo();
+            bug.AddInfo();
+            Assert.AreEqual(BugState.Analysis, bug.State);
+        }
+
+        [TestMethod]
+        public void Fix_FromAnalysis()
+        {
+            var bug = InAnalysis();
+            bug.Fix();
+            Assert.AreEqual(BugState.Fixed, bug.State);
+        }
+
+        [TestMethod]
+        public void NotBug_FromAnalysis()
+        {
+            var bug = InAnalysis();
+            bug.SetNotBug();
+            Assert.AreEqual(BugState.NotBug, bug.State);
+        }
+
+        [TestMethod]
+        public void NoFix_FromAnalysis()
+        {
+            var bug = InAnalysis();
+            bug.SetNoFix();
+            Assert.AreEqual(BugState.NoFix, bug.State);
+        }
+
+        [TestMethod]
+        public void Duplicate_FromAnalysis()
+        {
+            var bug = InAnalysis();
+            bug.SetDuplicate();
+            Assert.AreEqual(BugState.Duplicate, bug.State);
+        }
+
+        [TestMethod]
+        public void NotRepro_FromAnalysis()
+        {
+            var bug = InAnalysis();
+            bug.SetNotRepro();
+            Assert.AreEqual(BugState.NotRepro, bug.State);
+        }
+
+        [TestMethod]
+        public void Separate_FromAnalysis()
+        {
+            var bug = InAnalysis();
+            bug.SetSeparate();
+            Assert.AreEqual(BugState.Separate, bug.State);
+        }
+
+        [TestMethod]
+        public void NoTime_FromAnalysis()
+        {
+            var bug = InAnalysis();
+            bug.SetNoTime();
+            Assert.AreEqual(BugState.NoTime, bug.State);
+        }
+
+        [TestMethod]
+        public void Other_FromAnalysis()
+        {
+            var bug = InAnalysis();
+            bug.SetOther();
+            Assert.AreEqual(BugState.Other, bug.State);
+        }
+
+        [TestMethod]
+        public void AddInfo_FromNew_Throws()
+        {
+            var bug = new Bug();
+            Assert.ThrowsException<InvalidOperationException>(() => bug.AddInfo());
+        }
+
+        [TestMethod]
+        public void Fix_FromNew_Throws()
+        {
+            var bug = new Bug();
+            Assert.ThrowsException<InvalidOperationException>(() => bug.Fix());
+        }
+
+        [TestMethod]
+        public void Start_FromAnalysis_Throws()
+        {
+            var bug = InAnalysis();
+            Assert.ThrowsException<InvalidOperationException>(() => bug.Start());
+        }
+
+        [TestMethod]
+        public void AddInfo_FromAnalysis_Throws()
+        {
+            var bug = InAnalysis();
+            Assert.ThrowsException<InvalidOperationException>(() => bug.AddInfo());
+        }
+
+        [TestMethod]
+        public void NeedInfo_FromNeedInfo_Throws()
+        {
+            var bug = InNeedInfo();
+            Assert.ThrowsException<InvalidOperationException>(() => bug.RequestInfo());
+        }
+
+        [TestMethod]
+        public void Duplicate_FromNeedInfo_Throws()
+        {
+            var bug = InNeedInfo();
+            Assert.ThrowsException<InvalidOperationException>(() => bug.SetDuplicate());
+        }
+
+        [TestMethod]
+        public void Fixed_IsTerminal()
+        {
+            var bug = InAnalysis();
+            bug.Fix();
+            Assert.ThrowsException<InvalidOperationException>(() => bug.Start());
+        }
+
+        [TestMethod]
+        public void NotBug_IsTerminal()
+        {
+            var bug = InAnalysis();
+            bug.SetNotBug();
+            Assert.ThrowsException<InvalidOperationException>(() => bug.RequestInfo());
+        }
+
+        [TestMethod]
+        public void NoFix_IsTerminal()
+        {
+            var bug = InAnalysis();
+            bug.SetNoFix();
+            Assert.ThrowsException<InvalidOperationException>(() => bug.AddInfo());
+        }
+
+        [TestMethod]
+        public void Duplicate_IsTerminal()
+        {
+            var bug = InAnalysis();
+            bug.SetDuplicate();
+            Assert.ThrowsException<InvalidOperationException>(() => bug.Fix());
+        }
+
+        [TestMethod]
+        public void NotRepro_IsTerminal()
+        {
+            var bug = InAnalysis();
+            bug.SetNotRepro();
+            Assert.ThrowsException<InvalidOperationException>(() => bug.RequestInfo());
+        }
+
+        [TestMethod]
+        public void Separate_IsTerminal()
+        {
+            var bug = InAnalysis();
+            bug.SetSeparate();
+            Assert.ThrowsException<InvalidOperationException>(() => bug.AddInfo());
+        }
+
+        [TestMethod]
+        public void NoTime_IsTerminal()
+        {
+            var bug = InAnalysis();
+            bug.SetNoTime();
+            Assert.ThrowsException<InvalidOperationException>(() => bug.SetOther());
+        }
+
+        [TestMethod]
+        public void Other_IsTerminal()
+        {
+            var bug = InAnalysis();
+            bug.SetOther();
+            Assert.ThrowsException<InvalidOperationException>(() => bug.Fix());
+        }
+
+        [TestMethod]
+        public void Flow_Info_Back_ToAnalysis()
+        {
+            var bug = InAnalysis();
+            bug.RequestInfo();
+            bug.AddInfo();
+            Assert.AreEqual(BugState.Analysis, bug.State);
+        }
+
+        [TestMethod]
+        public void Flow_Info_To_Fixed()
+        {
+            var bug = InAnalysis();
+            bug.RequestInfo();
+            bug.AddInfo();
+            bug.Fix();
+            Assert.AreEqual(BugState.Fixed, bug.State);
+        }
+    }
+}
